4345f0e37dfbd3efb4144e38c6d55e5ffbb0a0f5
Ganeshh01 <gorantalaganesh01@gmail.com>
2025-04-26 14:43:24 +0530

    ganesh

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ function App() {
   return (
     <div className="App">
       <h1>Welcome to React</h1>
-      <p>hello ganesh</p>
+      <p>hello ganesh</p> 
+      <h1>Welcome to React</h1>
+      <p>hello ganesh</p> 
     </div>
   );
 }
