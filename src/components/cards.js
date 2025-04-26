4af716a93db6c0dffddbc192927a122f413c9f15
Ganeshh01 <gorantalaganesh01@gmail.com>
2025-04-26 15:34:54 +0530

    hover on cards

--- src/components/cards.js
+++ src/components/cards.js
@@ -6,18 +6,22 @@ const Cards = () => {
         <div className="card">
             <h2>Card 1</h2>
             <p>This is the first card.</p>
+            <button className='buttons'>Click Me</button>
         </div>
         <div className="card">
             <h2>Card 2</h2>
             <p>This is the second card.</p>
+            <button className='buttons'>Click Me</button>
         </div>
         <div className="card">
             <h2>Card 3</h2>
             <p>This is the third card.</p>
+            <button className='buttons'>Click Me</button>
         </div>
         <div className="card">
             <h2>Card 4</h2>
             <p>This is the third card.</p>
+            <button className='buttons'>Click Me</button>
         </div>
         </div>
     );
