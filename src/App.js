08281eec6c6025d54a1b4f2846b2078aed8071da
Ganeshh01 <gorantalaganesh01@gmail.com>
2025-04-26 15:15:05 +0530

    cards block

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import './App.css';
+import Cards from './components/cards';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to React</h1>
-      <p>hello ganesh</p> 
-      <h1>Welcome to React</h1>
-      <p>hello ganesh</p> 
+      <Cards/> 
     </div>
   );
 }
---
+++ src/components/cards.js
@@ -0,0 +1,26 @@
+import './cards.css'; // Import the CSS file for styling
+
+const Cards = () => {
+    return (
+        <div className="cards">
+        <div className="card">
+            <h2>Card 1</h2>
+            <p>This is the first card.</p>
+        </div>
+        <div className="card">
+            <h2>Card 2</h2>
+            <p>This is the second card.</p>
+        </div>
+        <div className="card">
+            <h2>Card 3</h2>
+            <p>This is the third card.</p>
+        </div>
+        <div className="card">
+            <h2>Card 4</h2>
+            <p>This is the third card.</p>
+        </div>
+        </div>
+    );
+}
+
+export default Cards;
